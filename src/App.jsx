3d83e299c35cf5d6860f3470490245deaf859093
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-19 11:43:25 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Slider App</h2>
+    </div>
+  )
+}
+
+export default App
