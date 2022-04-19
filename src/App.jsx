ec48468772a8c4d31691b03164cde6b04fd4fda8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-19 13:15:54 +0100

    Added slider and using useEffect to fix conflicting CSS stylings

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,23 @@ function App() {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
 
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000)
+    return () => clearInterval(slider)
+  }, [index])
+
   return (
     <div className="App">
       <section className="section">
